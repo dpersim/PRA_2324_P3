1c25dc2a1aaddeb695564df2c6e02f491bb51dfc
dpersim <dpersim@upv.edu.es>
2025-12-13 19:36:54 +0100

    Añadida implementación de la clase BSNode

---
+++ BSNode.h
@@ -0,0 +1,21 @@
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+template <typename T> 
+class BSNode {
+    public:
+        T* elem;    //Elemento almacenado en el nodo
+        BSNode<T>* left;    //Puntero a lado Izq
+        BSNode<T>* right;   //Puntero a lado Dch
+
+        BSNode(T elem, BSNode<T>* left=nullptr, BSNode<T>* right=nullptr):elem(elem),left(nullptr),right(nullptr) {}
+
+        friend std::ostream& operator<<(std::ostream &out, const BSNode<T> &bsn){
+            out<< bsn.elem;
+            return out;
+        }
+};
+
+#endif
